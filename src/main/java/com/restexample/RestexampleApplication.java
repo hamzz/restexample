962251908efab88e10aa9538b37a83d662bc8aca
Hamzah <hmz1988@bitbucket.org>
2016-12-22 10:21:46 +0700

    add cors filter

--- src/main/java/com/restexample/RestexampleApplication.java
+++ src/main/java/com/restexample/RestexampleApplication.java
@@ -3,12 +3,27 @@ package com.restexample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
 public class RestexampleApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins();
+			}
+		};
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestexampleApplication.class, args);
